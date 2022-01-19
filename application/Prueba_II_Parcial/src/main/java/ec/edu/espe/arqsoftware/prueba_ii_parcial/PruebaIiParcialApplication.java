e710b52b95caec131e94d365d585627f81387e4a
Jose Andres Zamora Miranda <jazamora4@espe.edu.ec>
2022-01-19 13:01:26 -0500

    Application

---
+++ application/Prueba_II_Parcial/src/main/java/ec/edu/espe/arqsoftware/prueba_ii_parcial/PruebaIiParcialApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arqsoftware.prueba_ii_parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaIiParcialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaIiParcialApplication.class, args);
+    }
+
+}
---
+++ application/Prueba_II_Parcial/src/test/java/ec/edu/espe/arqsoftware/prueba_ii_parcial/PruebaIiParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arqsoftware.prueba_ii_parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaIiParcialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
